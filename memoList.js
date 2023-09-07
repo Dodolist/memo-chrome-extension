ed785b4e69ae7a64a4ed3afd9d42b7d7c2e18623
Dodolist <dodolist3@gmail.com>
2023-09-07 15:27:12 +0900

    feat: 동기화 알림 표시

--- memoList.js
+++ memoList.js
@@ -15,16 +15,17 @@ addMemo.addEventListener('click', function () {
 var editedContent = '';
 var editedUpdatedAt = '';
 // autosaveTimer
-var autoSaveTimer;
+var autoSaveTimer = null;
 // memoData Object
 var memoData = {};
 
-
 inputTextarea.addEventListener('input', function () {
   clearTimeout(autoSaveTimer);
+  showLoading();
 
   // 메모를 2초 후에 스토리지에 저장
   autoSaveTimer = setTimeout(function () {
+
     if (editedUpdatedAt) {
       delete memoData[editedUpdatedAt];
     }
@@ -40,6 +41,8 @@ inputTextarea.addEventListener('input', function () {
     }
     saveMemoData();
     createMemoData();
+    completeLoading();
+    autoSaveTimer = null;
   }, 2000);
 });
 
@@ -172,3 +175,58 @@ function clearMemo() {
   showTopbar = false;
 }
 
+const memoBottomBar = document.querySelector('.memo-bottombar');
+
+const iconWrap = document.querySelector('.icon-wrap');
+const loadingIcon = document.querySelector('.loading-icon');
+const completeIcon = document.querySelector('.complete-icon');
+
+const textWrap = document.querySelector('.text-wrap');
+const syncText = document.querySelector('.sync-text');
+const loadingText = document.querySelector('.loading-text');
+
+function showLoading() {
+  iconWrap.style.transform = 'translateX(1px)';
+  syncText.style.transform = 'translateX(1px)';
+  loadingText.style.opacity = 1;
+  textWrap.style.color = '#00000000';
+
+  loadingIcon.style.scale = 1;
+  loadingIcon.style.opacity = 1;
+  completeIcon.style.scale = 0;
+  completeIcon.style.opacity = 0;
+
+  memoBottomBar.style.opacity = 1;
+}
+
+function completeLoading() {
+  iconWrap.style.transform = 'translateX(0px)';
+  syncText.style.transform = 'translateX(0px)';
+  loadingText.style.opacity = 0;
+  textWrap.style.color = '#00000080';
+
+  loadingIcon.style.scale = 0;
+  loadingIcon.style.opacity = 0;
+  completeIcon.style.scale = 1;
+  completeIcon.style.opacity = 1;
+  setTimeout(function () {
+    if(autoSaveTimer === null) {
+      hideLoading();
+    }
+  }, 1000);
+}
+
+function hideLoading() {
+  memoBottomBar.style.opacity = 0;
+  setTimeout(function () {
+    iconWrap.style.transform = 'translateX(1px)';
+    syncText.style.transform = 'translateX(1px)';
+    loadingText.style.opacity = 1;
+    textWrap.style.color = '#00000000';
+
+    loadingIcon.style.scale = 1;
+    loadingIcon.style.opacity = 1;
+    completeIcon.style.scale = 0;
+    completeIcon.style.opacity = 0;
+  }, 200);
+}
